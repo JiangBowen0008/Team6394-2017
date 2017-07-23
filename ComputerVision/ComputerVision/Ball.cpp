d8037cdf33375c32b9b1b6382c9add94aa985bae
Bowen Jiang <jbw0008@outlook.com>
2017-07-24 05:30:33 +0800

    最终程序

--- ComputerVision/ComputerVision/Ball.cpp
+++ ComputerVision/ComputerVision/Ball.cpp
@@ -8,10 +8,10 @@ using namespace cv;
 using namespace std;
 
 //反光条大小
-int AreaMax = 25;
+int AreaMax = 20;
 
 //反光条纵横比
-double RefleMaxRatio = 0.5;
+double RefleMaxRatio = 0.8;
 double RefleMinRatio = 0;
 
 //反光条间距-宽度比
@@ -19,40 +19,39 @@ int RefleInterRatio = 2.0;
 const int Max_RefleInterRatio = 5.0;
 
 Mat src; Mat src_gray;
-int thresh = 150;
+int thresh = 215;
 int max_thresh = 255;
 RNG rng(12345);
 
 /// 函数声明
 void thresh_callback(int, void*);
 double similar(int a, int b) {
-	return abs(double(a) - double(b)) / (double(a) + double(b));
+	return abs(double(a) - double(b)) / src.rows;
 }
 
 /** @主函数 */
 int main(int argc, char** argv)
 {
 	char* source_window = "Source";
-	namedWindow(source_window, CV_WINDOW_AUTOSIZE);
+	//namedWindow(source_window, CV_WINDOW_AUTOSIZE);
+	VideoCapture cap(1);
+	if (!cap.isOpened()) {
+		return -1;
+	}
 
 	/// 载入原图像, 返回3通道图像
-	src = imread("1.jpg", 1);
-	imshow(source_window, src);
-	//resize(src, src, Size(), 0.5, 0.5);
-	/// 转化成灰度图像并进行平滑
-
-	cvtColor(src, src_gray, CV_BGR2GRAY);
-
+	//src = imread("3.jpg", 1);
+	while (1) {
+		cap.read(src);
+		resize(src, src, Size(), 0.25, 0.25);
+		//imshow(source_window, src);
+		cvtColor(src, src_gray, CV_BGR2GRAY);
+		thresh_callback(0, 0);
+	}
+	
 	//blur(src_gray, src_gray, Size(3, 3));
-
 	/// 创建窗口
 
-
-	createTrackbar(" Threshold:", "Source", &thresh, max_thresh, thresh_callback);
-	createTrackbar(" 反光条间距:", "Source", &RefleInterRatio, Max_RefleInterRatio,  thresh_callback);
-	thresh_callback(0, 0);
-
-	waitKey(0);
 	return(0);
 }
 
@@ -65,8 +64,8 @@ void thresh_callback(int, void*)
 	vector<Vec4i> hierarchy;
 	/// 使用Threshold检测边缘
 	threshold(src_gray, threshold_output, thresh, 255, THRESH_BINARY);
-	namedWindow("ThresholdView");
-	imshow("ThresholdView", threshold_output);
+	//namedWindow("ThresholdView");
+	//imshow("ThresholdView", threshold_output);
 	/// 找到轮廓
 	findContours(threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
@@ -91,12 +90,12 @@ void thresh_callback(int, void*)
 		TmpRatio = (double(boundRect[i].height) / double(boundRect[i].width));
 		TmpArea= (double(boundRect[i].height) * double(boundRect[i].width));
 		if ((TmpRatio <= RefleMaxRatio) && (TmpRatio >= RefleMinRatio)&&(TmpArea>=AreaMax)) {
-			cout << TmpRatio << endl;
+			//cout << TmpRatio << endl;
 			RectFlag[i] = true;
 			area[i] = TmpArea;
 			color = Scalar(254, 0,0);
 			rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 3, 8, 0);
-			cout << "Find one" << endl;
+			//cout << "Find one" << endl;
 
 		}else RectFlag[i] = false;
 
@@ -107,7 +106,7 @@ void thresh_callback(int, void*)
 	//寻找荧光条
 	double MatchScore=2;
 	double TmpScore;
-	int area1, area2, x1, x2;
+	int area1, area2, x1, x2,h1,h2;
 	int r1 = -1;
 	int r2 = -1;
 
@@ -115,15 +114,18 @@ void thresh_callback(int, void*)
 	{
 		for (j = 0; j < contours.size(); j++)
 		{
-			if ((RectFlag[i] == true) && (RectFlag[j] == true)&&(i<j)) {
+			if ((RectFlag[i] == true) && (RectFlag[j] == true)&&(i!=j)) {
 				//TmpScore = abs((abs(boundRect[i].x - boundRect[j].x) / (boundRect[i].width + boundRect[j].width)) / RefleInterRatio - 1);
 				area1 = area[i];
 				area2 = area[j];
 				x1 = boundRect[i].x;
 				x2 = boundRect[j].x;
-				TmpScore = similar(area1,area2);
-				TmpScore = TmpScore*similar(x1,x2);
-				cout << "Score:" << TmpScore << endl;
+				h1 = boundRect[i].y;
+				h2 = boundRect[j].y;
+				TmpScore = similar(area1,1.5*area2);
+				TmpScore= TmpScore*similar(x1,x2);
+				TmpScore= TmpScore*similar(h1, h2+40);
+				//cout << "Score:" << TmpScore << endl;
 				if (TmpScore < MatchScore) {
 					MatchScore = TmpScore;
 					r1 = i;
@@ -132,7 +134,7 @@ void thresh_callback(int, void*)
 			}
 		}
 	}
-	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
+	//namedWindow("Contours", CV_WINDOW_AUTOSIZE);
 	for (i = 0; i < contours.size(); i++) {
 		color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
 		rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 1, 8, 0);
@@ -146,6 +148,7 @@ void thresh_callback(int, void*)
 		color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
 		rectangle(drawing, boundRect[r1].tl(), boundRect[r1].br(), color, 5, 8, 0);
 		rectangle(drawing, boundRect[r2].tl(), boundRect[r2].br(), color, 5, 8, 0);
+		cout << (2 * (boundRect[r1].x + boundRect[r2].x) + boundRect[r1].width + boundRect[r2].width) / 4-src.cols/2<<endl;
 		/*for (int i = 0; i< contours.size(); i++)
 		{
 		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
@@ -157,10 +160,10 @@ void thresh_callback(int, void*)
 		/// 显示在一个窗口
 	}
 	else {
-		cout << "None" << endl;
+		//cout << "None" << endl;
 
 	}
-	imshow("Contours", drawing);
+	//imshow("Contours", drawing);
 
 
 
